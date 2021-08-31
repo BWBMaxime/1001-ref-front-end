51614ab5890636dcef524e7838cf42acfc23f41c
Valentin <valentincreuillenet@hotmail.fr>
2021-08-31 14:24:21 +0000

    added Search Bar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <test-tailwind></test-tailwind>
-  <router-view></router-view>
+      <SearchBar></SearchBar>
+
 </template>
 <script>
 import TestTailwind from '@/components/TestTailwind.vue'
+import SearchBar from './components/SearchBar.vue'
 
 export default {
-  components: { TestTailwind },
-  TestTailwind
+  components: { TestTailwind, SearchBar },
+  TestTailwind,
+  SearchBar
 }
 </script>
 
