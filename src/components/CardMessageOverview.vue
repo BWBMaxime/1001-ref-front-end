981d777091b0bffcc075a71bf0f71d0be857e7a3
maialenwat <bwb.watrigant.m@fondespierre.com>
2021-09-01 10:35:06 +0000

    component card for overview of messages done, still need to implement function to truncate object

--- src/components/CardMessageOverview.vue
+++ src/components/CardMessageOverview.vue
@@ -1,68 +1,20 @@
 
 <template>
 
-<div class="w-84 float-left flex bg-grey dark:bg-gray-800 rounded-lg shadow mt-20 ml-20">
+<div class="float-left flex bg-gray dark:bg-gray-800 rounded-lg shadow mt-20 ml-20">
     <ul class="w-72 justify-start flex flex-col divide divide-y">
-        <li class="h-1/4 flex flex-row">
+        <li class="h-20 flex flex-row" v-for="message in messages" :key="message">
             <div class="select-none cursor-pointer flex flex-1 items-center p-4">
                 <div class="flex-1 pl-1 mr-16">
                     <div class="font-medium dark:text-white">
-                        Jean Michel
+                        {{ message.sender }}
                     </div>
                     <div class="text-gray-600 dark:text-gray-200 text-sm">
-                        Objet : Vendeur d'engrais
+                        <p>Objet : {{ message.object}} </p>
                     </div>
                 </div>
                 <div class="text-gray-600 dark:text-gray-200 text-xs">
-                    08:17
-                </div>
-            </div>
-        </li>
-        <li class="h-1/4 flex flex-row">
-            <div class="select-none cursor-pointer flex flex-1 items-center p-4">
-                <div class="flex-1 pl-1">
-                    <div class="font-medium dark:text-white">
-                        José Groseille
-                    </div>
-                    <div class="text-gray-600 dark:text-gray-200 text-sm">
-                        <p class="truncate text-overflow: ellipsis">
-                          Objet : J'aime les pommes de terres.
-                        </p>
-                    </div> 
-                </div>
-                <div class="text-gray-600 dark:text-gray-200 text-xs">
-                    09:02
-                </div>
-            </div>
-        </li>
-        <li class="h-1/4 flex flex-row">
-            <div class="select-none cursor-pointer flex flex-1 items-center p-4">
-                
-                <div class="flex-1 pl-1 mr-16">
-                    <div class="font-medium dark:text-white">
-                        Sylvie Reblochon
-                    </div>
-                    <div class="text-gray-600 dark:text-gray-200 text-sm">
-                        Objet : Bientôt la saison des tartiflettes.
-                    </div>
-                </div>
-                <div class="text-gray-600 dark:text-gray-200 text-xs">
-                    11:15
-                </div>
-            </div>
-        </li>
-        <li class="h-1/4 flex flex-row">
-            <div class="select-none cursor-pointer flex flex-1 items-center p-4">
-                <div class="flex-1 pl-1 mr-16">
-                    <div class="font-medium dark:text-white">
-                        Peio Irouleguy
-                    </div>
-                    <div class="text-gray-600 dark:text-gray-200 text-sm">
-                        Objet : Venez découvrir notre sélection vins.
-                    </div>
-                </div>
-                <div class="text-gray-600 dark:text-gray-200 text-xs">
-                    12:27
+                    {{ message.time }}
                 </div>
             </div>
         </li>
@@ -74,7 +26,27 @@
 
 <script>
 export default {
-
+  data(){
+    return {
+      messages: [
+        {
+            "sender": "Jean Michel",
+            "object": "Vendeur d'engrais",
+            "time": "11:15"
+        },
+        {
+            "sender": "José Groseille",
+            "object": "J'aime les pommes de terres",
+            "time": "12:27"
+        },
+        {
+            "sender": "Sylvie Reblochon",
+            "object": "Saison des tartiflettes",
+            "time": "09:18"
+        }
+      ]
+    }
+  }
 }
 </script>
 
