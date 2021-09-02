c27188570e92ce9d1c12e4cfb69b1dd0f676614c
Nordy34 <bwb.benomar.n@fondespierre.com>
2021-09-02 13:23:53 +0000

    fisrt-connection-modify

--- src/components/FirstCoDistributor.vue
+++ src/components/FirstCoDistributor.vue
@@ -2,7 +2,7 @@
 
 <main class="w-3/5 p-8 mx-auto">
         <p class="text-left md:text-center py4 text-4xl ">Bienvenue! C'est votre première connexion ! </p>
-           <p class="text-left md:text-center py-4 text-2xl extrabold ">Veuillez valider les 4 sections avant d'accéder à votre tableau de bord </p>
+           <p class="text-left md:text-center py-4 text-2xl extrabold ">Veuillez valider les 2 sections avant d'accéder à votre tableau de bord </p>
              <section class="shadow row">
                 <div class="tabs">
                     <div class="border-b tab">
@@ -131,7 +131,9 @@
                                    </form>
                                 </div>
                             </div>
-              
+
+
+                            
                         </div>
                     </div>
 
@@ -141,7 +143,7 @@
                             <input class="w-full absolute z-10 cursor-pointer opacity-0 h-5 top-6" type="checkbox" id="chck2">
                             <header class="flex justify-between items-center p-5 pl-8 pr-8 cursor-pointer select-none tab-label" for="chck2">
                                 <span class="text-grey-darkest font-thin text-xl">
-                                  2 - Information sur ma production
+                                  2 - Information sur mon entreprise
                                 </span>
                                 <div class="rounded-full border border-grey w-7 h-7 flex items-center justify-center test">
                                     <!-- icon by feathericons.com -->
@@ -176,77 +178,6 @@
                         </div>
                     </div>
                 
-                                        <div class="border-b tab">
-                        <div class="border-l-2 border-transparent relative">
-                            <input class="w-full absolute z-10 cursor-pointer opacity-0 h-5 top-6" type="checkbox" id="chck3">
-                            <header class="flex justify-between items-center p-5 pl-8 pr-8 cursor-pointer select-none tab-label" for="chck3">
-                                <span class="text-grey-darkest font-thin text-xl">
-                                    3 - Inviter mes 5 premiers clients
-                                </span>
-                                <div class="rounded-full border border-grey w-7 h-7 flex items-center justify-center test">
-                                    <!-- icon by feathericons.com -->
-                                    <svg aria-hidden="true" class="" data-reactid="266" fill="none" height="24" stroke="#606F7B" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                                        <polyline points="6 9 12 15 18 9">
-                                        </polyline>
-                                    </svg>
-                                </div>
-                            </header>
-                            <div class="tab-content">
-                                <div class="pl-8 pr-8 pb-5 text-grey-darkest">
-                                    <form class="space-y-3 text-gray-700">
-                                         <div class="w-full px-3">
-                                            <label class=" text-gray-700 text-sm font-bold mb-2" for="grid-password">
-                                                Client 1
-                                            </label>
-                                            <input class="appearance-none block w-full bg-white-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-yellow-500" id="email" type="email" placeholder="E-mail">
-                                            </div>
-
-
-                                                <div class="w-full px-3">
-                                            <label class="text-gray-700 text-sm font-bold mb-2" for="grid-password">
-                                                Client 2
-                                            </label>
-                                            <input class="appearance-none block w-full bg-white-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-yellow-500" id="email" type="email" placeholder="E-mail">
-                                            </div>
-
-
-                                                <div class="w-full px-3">
-                                            <label class="text-gray-700 text-sm font-bold mb-2" for="grid-password">
-                                                Client 3
-                                            </label>
-                                            <input class="appearance-none block w-full bg-white-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-yellow-500" id="email" type="email" placeholder="E-mail">
-                                            </div>
-
-
-                                                <div class="w-full px-3">
-                                            <label class=" text-gray-700 text-sm font-bold mb-2" for="grid-password">
-                                                Client 4
-                                            </label>
-                                            <input class="appearance-none block w-full bg-white-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-yellow-500" id="email" type="email" placeholder="E-mail">
-                                            </div>
-
-                                            
-                                                <div class="w-full px-3">
-                                            <label class="text-gray-700 text-sm font-bold mb-2" for="grid-password">
-                                                Client 5
-                                            </label>
-                                            <input class="appearance-none block w-full bg-white-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-yellow-500" id="email" type="email" placeholder="E-mail">
-                                            </div>
-
-
-
-                                                <!--Boutton enregistrer-->  
-                                                <div class="py-4 mb-1 text-center">
-                                                <button class="transition duration-500 bg-yellow-500 hover: 0 text-white font-bold py-2 px-8 rounded focus:outline-none focus:shadow-outline" type="submit">
-                                                Enregistrer
-                                                </button>
-                                                </div>
-                                    </form>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </section>
         </main>
