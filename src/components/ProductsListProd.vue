31d368af632e44ffe94d207463d495694afb86ea
maialenwat <bwb.watrigant.m@fondespierre.com>
2021-09-14 21:30:35 +0000

    binding with backend functionnal

--- src/components/ProductsListProd.vue
+++ src/components/ProductsListProd.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="container mx-auto px-24 py-8 font-mono flex items-center justify-center">
-        <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
+        <div class="w-full mb-8 overflow-hidden rounded shadow-lg">
             <!-- Table of products -->
             <div class="w-full 2xl:overflow-hidden overflow-x-auto">
                 <div class="flex mt-6 mr-6 items-center pb-5 border-gray-100">
@@ -12,11 +12,11 @@
                 <table class="w-full border-separate place-self-center">
                     <h2 class="mb-5 text-xl text-yellow-500 font-semibold uppercase flex justify-center mx-auto">Vos produits (#)</h2>
                     <thead class="opacity-80 bg-yellow-500 flex text-white w-full">
-                        <tr class="flex w-full text-center">
-                            <th class="px-4 py-3 w-96">Produit</th>
-                            <th class="px-4 py-3 w-36 ml-2">Prix</th>
+                        <tr class="flex w-full text-center justify-between">
+                            <th class="px-4 py-3 w-80">Produit</th>
+                            <th class="px-4 py-3 w-36 ml-2">Prix Revendeur</th>
+                            <th class="px-4 py-3 w-36 ml-2">Prix Distributeur</th>
                             <th class="px-4 py-3 w-24">Nouveau</th>
-                            <th class="px-4 py-3 w-24">Promotion</th>
                             <th class="px-4 py-3 w-24">Affiché</th>
                             <th class="px-2 py-3 w-48">Modif./ Suppression</th>
                         </tr>
@@ -26,8 +26,8 @@
                         <div v-for="product in products" v-bind:key="product">
                             <tr class="text-gray-700 h-auto" v-for="variation in product.variations" v-bind:key="variation">
                                 <!-- Product Name -->
-                                <td class=" mx-4 my-3 border">
-                                    <div class="w-96 flex items-center text-sm">
+                                <td class="pt-3 border">
+                                    <div class="w-80 flex items-center text-sm">
                                         <div class="relative w-8 h-8 mx-3 rounded-full md:block inline-flex">
                                             <img class="object-cover w-full h-full rounded-full" src="https://cdn.pixabay.com/photo/2017/01/21/21/15/beer-1998293_960_720.jpg" alt="" loading="lazy">
                                             <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
@@ -37,35 +37,35 @@
                                         </div>
                                     </div>
                                 </td>
-                                <!-- Price -->
-                                <td class="mx-4 my-3 w-36 text-sm border text-center">{{ variation.prixRevendeur }} €</td>
+
+                                <!-- Dealer Price -->
+                                <td class="mx-4 my-3 w-48 text-sm border text-center">{{ variation.dealerPrice }} €</td>
+
+                                <!-- Restaurateur Price -->
+                                <td class="mx-4 my-3 w-48 text-sm border text-center">{{ variation.restaurateurPrice }} €</td>
+
                                 <!-- New -->
-                                <td v-if="product.new == true" class="mx-4 my-3 w-24 text-sm border text-center">
+                                <td v-if="product.new == true" class="w-32 text-sm border text-center">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                                     </svg>
                                 </td>
-                                <td v-else class="mx-4 my-3 w-48 text-sm border text-center"></td>
-                                <!-- Promo -->
-                                <td v-if="product.promo == true" class="mx-4 my-3 w-24 text-sm border text-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                    </svg>
-                                </td>
-                                <td v-else class="mx-4 my-3 w-48 text-sm border text-center"></td>
+                                <td v-else class="w-32 text-sm border text-center"></td>
+
                                 <!-- Display -->
-                                <td class="mx-4 w-48 text-sm border-2 text-center">
+                                <td class="w-32 text-sm border-2 text-center">
                                     <input v-if="product.display == true" class="h-4 w-4" type="radio" checked>
                                     <input v-else class="h-4 w-4" type="radio">
                                 </td>
+
                                 <!-- Edit/Delete -->
-                                <td class="w-48 pl-12 py-3 text-xs border text-center flex items-center">
-                                    <span class="font-semibold leading-tight rounded-sm w-full">
+                                <td class="py-6 w-48 text-sm border text-center flex justify-evenly">
+                                    <span class="font-semibold leading-tight rounded-sm">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                         </svg>
                                     </span>
-                                    <span class="font-semibold leading-tight rounded-sm w-full">
+                                    <span class="font-semibold leading-tight rounded-sm">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                         </svg>
@@ -89,32 +89,35 @@ export default {
 
         let products = 
         [{
-            userId:1,
             name: "",
             category:"",
             description:"",
+            tags:[],
             photo:"",
             active:false,
             new:false,
             variations: 
             [{
-                contenant:"", 
-                conditionnement:"",
-                contenance:"",
-                prixRevendeur:"",
-                prixRestaurateur:""
+                container:"", 
+                conditionning:"",
+                capacity:"",
+                dealerPrice:"",
+                restaurateurPrice:""
             }],
-            tags: []
+            id:1
         }]
         
-        return products;
+        return {products}
         
     },
     
 
-    methods:{
+    methods: {
+
         getAllProductsByUserId(){
-            ProductController.getCurrentUserProducts(1);
+            console.log("products to hydrate : ");
+            console.log(this.products);
+            ProductController.getCurrentUserProducts(this.$store.state.currentUser.id, this.products);
         },
 
         /** 
@@ -127,10 +130,15 @@ export default {
         /**
          * deletes product at given index from the list
          */
-        // deleteProduct(){
-            
+        // deleteProduct(index){
+        //     this.product.splice(index, 1);
         // }
-    }
+    },
+
+    beforeMount() {
+        console.log(this.products);
+        this.getAllProductsByUserId();
+    },
 }
 
 
--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,8 +1,10 @@
+import { hydrate } from '@vue/runtime-dom';
 import axios from 'axios';
+
 
 const ProductController = {
     saveProduct(product) {
-        axios.post('http://127.0.0.1:8000/product/create', JSON.stringify(product), {withCredentials:false})
+        axios.post('http://localhost:8000/product/create', JSON.stringify(product), {withCredentials:false})
         .then(function(response){
         console.log(response.data);
         })
@@ -12,24 +14,35 @@ const ProductController = {
     },
 
     
-    getCurrentUserProducts(id){
-        console.log(JSON.stringify(id));
-        axios.get('http://localhost:8000/getProducts/' + id, JSON.stringify(id), {withCredentials:false})
+    getCurrentUserProducts(id, products){
+        console.log(id);
+        axios.get('http://localhost:8000/getProducts/' + id, {withCredentials:false})
         .then(function(response){
             console.log(response.data);
-            console.log(response.status);
-            console.log(response.statusText);
-            console.log(response.headers);
-            console.log(response.config);
+            hydratePage(products, response.data);
         })
         .catch(error => {
-            this.errorMessage = error.message;
-            console.error("There was an error!", error);
-            // using toJSON you get an object with more information about the HTTP error
-            console.log(error.toJSON());
+            
+            console.log(error);
         });
-    }
+    },
 
 }
+
+function hydratePage(products, data){
+
+    console.log("product to hydrate : " + products);
+    console.log("data to hydrate : " + data);
+
+
+    for(let keys in data){
+        if(data[keys] != null) products[keys] = data[keys];
+    }
+
+    products.loaded = true;
+
+    console.log(products);
+}
+
 
 export default ProductController;
