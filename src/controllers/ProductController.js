43851d41b9db5e40a7f09dffa1070f49443215c3
maialenwat <bwb.watrigant.m@fondespierre.com>
2021-09-16 12:57:30 +0000

    delete button operational

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -3,22 +3,28 @@ import axios from 'axios';
 
 
 const ProductController = {
+
+    /**
+     * saves a new product in database 
+     */
     saveProduct(product) {
         axios.post('http://localhost:8000/product/create', JSON.stringify(product), {withCredentials:false})
         .then(function(response){
-        console.log(response.data);
+        // console.log(response.data);
         })
         .catch(error => {
             console.log(error)
         })
     },
 
-    
+    /**
+     * get all products from current user
+     */
     getCurrentUserProducts(id, products){
-        console.log(id);
+        // console.log(id);
         axios.get('http://localhost:8000/getProducts/' + id, {withCredentials:false})
         .then(function(response){
-            console.log(response.data);
+            // console.log(response.data);
             hydratePage(products, response.data);
         })
         .catch(error => {
@@ -27,12 +33,31 @@ const ProductController = {
         });
     },
 
+
+    /**
+     * delete a product variation from database by its id
+     */
+    deleteVariationById(id){
+        axios.delete('http://localhost:8000/variation/delete/' + id, {withCredentials:false})
+        .then(function(response){
+            // console.log(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    },
+
 }
 
+/**
+ * fills the tab with the right datas 
+ * @param {*} products
+ * @param {*} data
+ */
 function hydratePage(products, data){
 
-    console.log("product to hydrate : " + products);
-    console.log("data to hydrate : " + data);
+    // console.log("product to hydrate : " + products);
+    // console.log("data to hydrate : " + data);
 
 
     for(let keys in data){
